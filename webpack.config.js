0d1f63e781784cc7649f375f2f5cd42acd195c9d
Doplax <doplax@gmail.com>
2023-11-04 15:34:15 +0100

    After moving the logic from /js to /src with their corresponding entry points.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 module.exports = {
     entry: {
-        index: './src/index.js',
-        app: './src/app.js', 
+        home: './src/homePage.js',
+        teams: './src/teamsPage.js', 
+        contact: './src/contactPage.js'
     },
     mode: 'development',
+    devtool: 'inline-source-map', // Nos dice en que archivo y en que linea
     output: {
         filename: '[name].bundle.js',
-        path: __dirname + '/dist',
-        clean: true,
+        path: __dirname + '/dist', // __dirname: Es una variable que resuelve la ruta absoluta donde estemos en este momento
+        clean: true,// clean: Limpiara el directorio antes de manipularlo
     }
 }
 
-// __dirname: Es una variable que resuelve la ruta absoluta donde estemos en este momento
-// clean: Limpiara el directorio antes de manipularlo
